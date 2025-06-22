e0023d725c0676facf04de9f6b83d493328dd438
Jhana Schiano <jhana.schiano@dbsconsulting.biz>
2025-06-22 21:50:28 +0200

    struttura file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//chiamare l'api esterna per ottenre una lista di libri in base alla categoria -> servizio axios o lodash (?)
+
+//dovrò salvarle su una variabile d'ambiente
+
+// deve essere paginato / dare la possibilità di caricare più file
+
+// al click devo chiamare un'alttra api per ottenere i dettagli del libro
+
+//buona struttura del progetto
+
+//webpack 
